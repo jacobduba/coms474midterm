865a6dd8bd7762f66ef2f07be63cdaa42f7b3da8
Jose Rodrigo Santamaria Castro <jrodsc@mac-air-139.cs.iastate.edu>
2025-03-28 17:08:20 -0500

    Adding svm and decision tree to analysis.py

--- analysis.py
+++ analysis.py
@@ -13,6 +13,18 @@ print(df.head())
 
 # SVM
 
+svm = SVC(kernel='poly')
+svm.fit(X_train_scaled_unscaled, y_train)
+svm_prediction = svm.predict(X_test_scaled_unscaled)
+svm_accuracy = accuracy_score(y_test, svm_prediction)
+print(f"SVM score: #{svm_accuracy}")
+
 # Decision Tree
+
+dt = DecisionTreeClassifier()
+dt.fit(X_train_scaled_unscaled, y_train)
+dt_prediction = clf.predict(X_test_scaled_unscaled)
+dt_accuracy = accuracy_score(y_test, clf_prediction)
+print(f"Decision Tree score: #{dt_accuracy}")
 
 # k-NN

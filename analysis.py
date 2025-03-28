6df98ff3c624b4a1bfc208b42fe31254f08ce9d3
Jacob Duba <jacob@mooshoe.me>
2025-03-28 13:49:12 -0500

    Processed data

--- analysis.py
+++ analysis.py
@@ -1,12 +1,31 @@
 import pandas as pd
+import numpy as np
+from numpy.typing import NDArray
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv('schizophrenia_dataset.csv')
 
-X = df[['Age', 'Gender', 'Education_Level', 'Marital_Status', 'Occupation', 'Income_Level', 'Place_of_Residence', 'Family_History_of_Schizophrenia', 'Substance_Use', 'Suicide_Attempt', 'Positive_Symptom_Score', 'Negative_Symptom_Score', 'GAF_Score', 'Social_Support', 'Stress_Factors', 'Medication_Adherence', 'Medication_Adherence']]
+scaled_cols = ['Age', 'Positive_Symptom_Score', 'Negative_Symptom_Score', 'GAF_Score']
+unscaled_cols = ['Gender', 'Education_Level', 'Marital_Status', 'Income_Level', 'Occupation', 'Place_of_Residence', 'Family_History_of_Schizophrenia', 'Substance_Use', 'Suicide_Attempt', 'Social_Support', 'Stress_Factors', 'Medication_Adherence']
+
+X = df[scaled_cols + unscaled_cols]
 y = df['Diagnosis']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+X_train_to_scale = X_train[scaled_cols]
+X_test_to_scale = X_test[scaled_cols]
+
+scaler = StandardScaler()
+X_train_scaled: NDArray[np.float64] = scaler.fit_transform(X_train_to_scale)
+X_test_scaled: NDArray[np.float64] = scaler.transform(X_test_to_scale)
+
+X_train_unscaled: NDArray[np.float64] = X_train[unscaled_cols].to_numpy()
+X_test_unscaled: NDArray[np.float64] = X_test[unscaled_cols].to_numpy()
+
+X_train_scaled_unscaled = np.concatenate((X_train_scaled, X_train_unscaled), axis=1)
+X_test_scaled_unscaled = np.concatenate((X_test_scaled, X_test_unscaled), axis=1)
 
 # Linear Perceptron (if need more models)
 

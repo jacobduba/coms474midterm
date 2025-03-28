ab4356927c479e4d0b47a049a2e5eb1e3970c058
Jacob Duba <jacob@mooshoe.me>
2025-03-27 19:46:34 -0500

    awesome sauce research

--- analysis.py
+++ analysis.py
@@ -4,7 +4,12 @@ df = pd.read_csv('schizophrenia_dataset.csv')
 
 print(df.head())
 
-# Perceptron
+# We can caluclate probabilities for all of these
+# SVM (Platt scaling)
+# Decision tree returns percent of classifiers at end
+# Like k-NN gives the percentage of nearest neighbors.
+
+# Logistic Regression
 
 # SVM
 

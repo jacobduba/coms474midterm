11a5c228fcb0cea73b936268f4f8a0e51d45c42b
Jacob Duba <jacob@mooshoe.me>
2025-03-28 11:32:01 -0500

    Parse data

--- analysis.py
+++ analysis.py
@@ -1,18 +1,19 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('schizophrenia_dataset.csv')
 
-print(df.head())
+X = df[['Age', 'Gender', 'Education_Level', 'Marital_Status', 'Occupation', 'Income_Level', 'Place_of_Residence', 'Family_History_of_Schizophrenia', 'Substance_Use', 'Suicide_Attempt', 'Positive_Symptom_Score', 'Negative_Symptom_Score', 'GAF_Score', 'Social_Support', 'Stress_Factors', 'Medication_Adherence', 'Medication_Adherence']]
+y = df['Diagnosis']
 
-# We can caluclate probabilities for all of these
-# SVM (Platt scaling)
-# Decision tree returns percent of classifiers at end
-# Like k-NN gives the percentage of nearest neighbors.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Linear Perceptron (if need more models)
 
 # Logistic Regression
 
 # SVM
 
-# Decision Tree
+# Decision Tree (if need more models)
 
 # k-NN

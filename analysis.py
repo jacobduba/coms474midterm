c3e6696be209dc333b91e0106411245d2d14d6e5
Omar Muhammetkulyyev <omar99@ky496g37k7.engl.iastate.edu>
2025-03-28 21:52:26 -0500

    added more metrics

--- analysis.py
+++ analysis.py
@@ -6,13 +6,15 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
 df = pd.read_csv('schizophrenia_dataset.csv')
 
 scaled_cols = ['Age', 'Positive_Symptom_Score', 'Negative_Symptom_Score', 'GAF_Score']
-unscaled_cols = ['Patient_ID', 'Duration_of_Illness', 'Number_of_Hospitalizations', 'Gender', 'Education_Level', 'Marital_Status', 'Income_Level', 'Occupation', 'Place_of_Residence', 'Family_History_of_Schizophrenia', 'Substance_Use', 'Suicide_Attempt', 'Social_Support', 'Stress_Factors', 'Medication_Adherence']
+unscaled_cols = ['Number_of_Hospitalizations', 'Gender', 'Education_Level', 'Marital_Status', 'Income_Level', 'Occupation', 'Place_of_Residence', 'Family_History_of_Schizophrenia', 'Substance_Use', 'Suicide_Attempt', 'Social_Support', 'Stress_Factors', 'Medication_Adherence']
 
 X = df[scaled_cols + unscaled_cols]
 y = df['Diagnosis']
@@ -39,14 +41,21 @@ lr.fit(X_train_scaled_unscaled, y_train)
 lr_prediction = lr.predict(X_test_scaled_unscaled)
 lr_accuracy = accuracy_score(y_test, lr_prediction)
 print(f"Logistic Regression score: #{lr_accuracy}")
+precision = precision_score(y_test, lr_prediction, average='macro')
+print(f"Precision: {precision}")
+recall = recall_score(y_test, lr_prediction, average='macro')
+print(f"Recall: {recall}")
 
 # SVM
-
 svm = SVC(kernel='poly')
 svm.fit(X_train_scaled_unscaled, y_train)
 svm_prediction = svm.predict(X_test_scaled_unscaled)
 svm_accuracy = accuracy_score(y_test, svm_prediction)
 print(f"SVM score: #{svm_accuracy}")
+precision = precision_score(y_test, svm_prediction, average='macro')
+print(f"Precision: {precision}")
+recall = recall_score(y_test, svm_prediction, average='macro')
+print(f"Recall: {recall}")
 
 # Decision Tree
 
@@ -55,8 +64,20 @@ dt.fit(X_train_scaled_unscaled, y_train)
 dt_prediction = dt.predict(X_test_scaled_unscaled)
 dt_accuracy = accuracy_score(y_test, dt_prediction)
 print(f"Decision Tree score: #{dt_accuracy}")
+precision = precision_score(y_test, dt_prediction, average='macro')
+print(f"Precision: {precision}")
+recall = recall_score(y_test, dt_prediction, average='macro')
+print(f"Recall: {recall}")
 
 # k-NN
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X_train_scaled_unscaled, y_train)
-print(f"KNN Score: #{knn.score(X_test_scaled_unscaled, y_test)}")
+knn_prediction = knn.predict(X_test_scaled_unscaled)
+knn_accuracy = accuracy_score(y_test, knn_prediction)
+print(f"KNN score: #{knn_accuracy}")
+# print(f"KNN Score: #{knn.score(X_test_scaled_unscaled, y_test)}")
+
+precision = precision_score(y_test, knn_prediction, average='macro')
+print(f"Precision: {precision}")
+recall = recall_score(y_test, knn_prediction, average='macro')
+print(f"Recall: {recall}")
